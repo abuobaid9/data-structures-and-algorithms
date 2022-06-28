e453497671caed35e2da92519acd168ab77ce678
Anas Abuobaid <abuobaid.anas1999@gmail.com>
2022-06-28 19:43:09 +0300

    PseudoQueue

--- javascript/stack-queue-pseudo/__test__/index.test.js
+++ javascript/stack-queue-pseudo/__test__/index.test.js
@@ -7,68 +7,21 @@ describe('Stac And Queue', () => {
   it('works', () => {
     expect(true).toBeTruthy();
   });
-  test('Can successfully push onto a stack', () => {
-    const newQueue = new Queue();
-    newQueue.push(1);
-    const result = newQueue.peek();
-    expect(result).toEqual("Exception");
-
-  });
   test('Can successfully push multiple values onto a stack', () => {
     const newQueue = new Queue();
-    newQueue.push(1);
-    newQueue.push(2);
-    newQueue.push(3);
-    newQueue.push(4);
-    newQueue.push('a');
-    const result = newQueue.peek();
-    expect(result).toEqual( "Exception");
+    newQueue.enqueue(1);
+    newQueue.enqueue(2);
+    newQueue.enqueue(3);
+    newQueue.enqueue(4);
+    newQueue.enqueue('a');
+    const result = newQueue.stack1.top.value;
+    expect(result).toEqual('a');
 
-  });
-
-  test('Can successfully pop off the stack', () => {
-    const newQueue = new Queue();
-    newQueue.push(1);
-    newQueue.push(2);
-    newQueue.push(3);
-    newQueue.push(4);
-    newQueue.push('a');
-    newQueue.pop();
-    const result = newQueue.peek();
-    expect(result).toEqual("Exception");
-
-  });
-  test('Can successfully empty a stack after multiple pops', () => {
-    const newQueue = new Queue();
-    newQueue.push(1);
-    newQueue.push('a');
-    newQueue.pop();
-    newQueue.pop();
-    newQueue.pop();
-    const result = newQueue.peek();
-    expect(result).toEqual('Exception');
-
-  });
-
-  test('Can successfully peek the next item on the stack', () => {
-    const newQueue = new Queue();
-    newQueue.push(1);
-    newQueue.push(2);
-    newQueue.push(3);
-    const result = newQueue.peek();
-    expect(result).toEqual( "Exception");
-  });
-  it('Calling pop or peek on empty stack raises exception', () => {
-    const newQueue = new Queue();
-    const result = newQueue.peek();
-    const result2 = newQueue.pop();
-    expect(result).toEqual('Exception');
-    expect(result2).toEqual('Exception');
   });
   test('Can successfully enqueue into a queue', () => {
     const newQueue = new Queue();
     newQueue.enqueue(1);
-    const result = newQueue.peek();
+    const result = newQueue.stack1.top.value;
     expect(result).toEqual(1);
   });
   test('Can successfully enqueue multiple values into a queue', () => {
@@ -76,8 +29,8 @@ describe('Stac And Queue', () => {
     newQueue.enqueue(1);
     newQueue.enqueue(2);
     newQueue.enqueue(3);
-    const result = newQueue.peek();
-    expect(result).toEqual(1);
+    const result = newQueue.stack1.top.value;
+    expect(result).toEqual(3);
   });
   test('Can successfully dequeue out of a queue the expected value', () => {
     const newQueue = new Queue();
@@ -85,16 +38,7 @@ describe('Stac And Queue', () => {
     newQueue.enqueue(2);
     newQueue.enqueue(3);
     newQueue.dequeue();
-    const result = newQueue.peek();
-    expect(result).toEqual(2);
-  });
-  test('Can successfully peek into a queue, seeing the expected value', () => {
-    const newQueue = new Queue();
-    newQueue.enqueue(1);
-    newQueue.enqueue(2);
-    newQueue.enqueue(3);
-    newQueue.dequeue();
-    const result = newQueue.peek();
+    const result = newQueue.stack1.top.value;
     expect(result).toEqual(2);
   });
   test('Can successfully empty a queue after multiple dequeues', () => {
@@ -106,18 +50,18 @@ describe('Stac And Queue', () => {
     newQueue.dequeue();
     newQueue.dequeue();
     newQueue.dequeue();
-    const result = newQueue.peek();
-    expect(result).toEqual('Exception');
+    const result = newQueue.stack1.top;
+    expect(result).toEqual(null);
   });
   it('Can successfully instantiate an empty queue', () => {
     let newQueue = new Queue();
-    expect(newQueue.front).toBeNull();
+    expect(newQueue.stack1.top).toBeNull();
   });
-  it('Calling dequeue or peek on empty queue raises exception', () => {
+  it('Calling dequeue on empty queue raises exception', () => {
     const newQueue = new Queue();
-    const result = newQueue.peek();
+    // const result = newQueue.stack1.top;
     const result2 = newQueue.dequeue();
-    expect(result).toBe('Exception');
+    // expect(result).toBe('Exception');
     expect(result2).toBe('Exception');
   });
 });
--- javascript/stack-queue-pseudo/lib/pseudoQueue.js
+++ javascript/stack-queue-pseudo/lib/pseudoQueue.js
@@ -1,71 +1,23 @@
 'use strict';
-const Node = require('./Node');
+const Stack = require('./Stack');
+
 class PseudoQueue {
   constructor() {
-    this.front = null;
-    this.rear = null;
-    this.length = 0;
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
   }
-  isEmpty() {
-    if (this.front === null || this.length === 0) {
-      return true;
-    } else {
-      return false;
-    }
 
-  }
   enqueue(value) {
-
-    const newNode = new Node(value);
-    if (this.isEmpty()) {
-      this.front = newNode;
-      this.rear = newNode;
-      this.length += 1;
-    } else {
-      this.rear.next = newNode;
-      this.rear = newNode;
-      this.length += 1;
-    }
+    this.stack1.push(value);
   }
+
   dequeue() {
-
-    if (this.isEmpty()) {
-      return 'Exception';
+    if (this.stack1.top) {
+      this.stack2.push(this.stack1.top.value);
+      return this.stack1.pop();
     } else {
-      const temp = this.front;
-      this.front = this.front.next;
-      temp.next = null;
-      this.length -= 1;
-      return temp.value;
+      return "Exception";
     }
-  }
-  push(value) {
-    const newNode = new Node(value);
-    if (this.isEmpty()) {
-      this.top = newNode;
-      this.length += 1;
-    } else {
-      newNode.next = this.top;
-      this.top = newNode;
-      this.length += 1;
-    }
-  }
-  pop() {
-    if (this.isEmpty()) {
-      return 'Exception';
-    }
-    const temp = this.top;
-    this.top = this.top.next;
-    temp.next = null;
-    this.length -= 1;
-    return temp.value;
-  }
-  peek() {
-    // i do care only about the front
-    if (this.isEmpty()) {
-      return 'Exception';
-    }
-    return this.front.value;
   }
 
 }
