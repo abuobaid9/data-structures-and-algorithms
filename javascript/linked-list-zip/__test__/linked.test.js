69ce3f8b79373e116fbee376132dd06b3953dbd7
Anas Abuobaid <abuobaid.anas1999@gmail.com>
2022-07-03 17:19:33 +0300

    edit

--- javascript/linked-list-zip/__test__/linked.test.js
+++ javascript/linked-list-zip/__test__/linked.test.js
@@ -1,83 +1,83 @@
-// 'use strict';
+'use strict';
 
-// // Require linked list implementation
+// Require linked list implementation
 // const LinkedList = require('../index');
 
 
 
-// describe('Linked List', () => {
-//   let p = null, q = null;
-//   it('works', () => {
-//     expect(true).toBeTruthy();
-//   });
-//   test('Zip two lists together equal ', () => {
-//     let ll = new LinkedList();
-//     p = ll.push(p, 3);
-//     p = ll.push(p, 2);
-//     p = ll.push(p, 1);
-//     ll.printList(p);
-//     q = ll.push(q, 6);
-//     q = ll.push(q, 5);
-//     q = ll.push(q, 4);
-//     ll.printList(q);
-//     q =ll.zipLists(p, q);
-//     ll.printList(p);
-//     ll.printList(q);
+describe('Linked List', () => {
+  // let p = null, q = null;
+  it('works', () => {
+    expect(true).toBeTruthy();
+  });
+  // test('Zip two lists together equal ', () => {
+  //   let ll = new LinkedList();
+  //   p = ll.push(p, 3);
+  //   p = ll.push(p, 2);
+  //   p = ll.push(p, 1);
+  //   ll.printList(p);
+  //   q = ll.push(q, 6);
+  //   q = ll.push(q, 5);
+  //   q = ll.push(q, 4);
+  //   ll.printList(q);
+  //   q =ll.zipLists(p, q);
+  //   ll.printList(p);
+  //   ll.printList(q);
 
-//     expect(ll.printList()).toEqual(/*1
-//     2
-//     3
-//     <br>
-//     4
-//     5
-//     6
-//     7
-//     8
-//     <br>
-//     1
-//     4
-//     2
-//     5
-//     3
-//     6
-//     <br> */);
-//   });
-//   test('Zip two lists together not equal ', () => {
-//     let ll = new LinkedList();
-//     p = ll.push(p, 3);
-//     p = ll.push(p, 2);
-//     p = ll.push(p, 1);
-//     ll.printList(p);
-//     q = ll.push(q, 8);
-//     q = ll.push(q, 7);
-//     q = ll.push(q, 6);
-//     q = ll.push(q, 5);
-//     q = ll.push(q, 4);
-//     ll.printList(q);
-//     q =ll.zipLists(p, q);
-//     ll.printList(p);
-//     ll.printList(q);
+  //   expect(ll.printList()).toEqual(/*1
+  //   2
+  //   3
+  //   <br>
+  //   4
+  //   5
+  //   6
+  //   7
+  //   8
+  //   <br>
+  //   1
+  //   4
+  //   2
+  //   5
+  //   3
+  //   6
+  //   <br> */);
+  // });
+  // test('Zip two lists together not equal ', () => {
+  //   let ll = new LinkedList();
+  //   p = ll.push(p, 3);
+  //   p = ll.push(p, 2);
+  //   p = ll.push(p, 1);
+  //   ll.printList(p);
+  //   q = ll.push(q, 8);
+  //   q = ll.push(q, 7);
+  //   q = ll.push(q, 6);
+  //   q = ll.push(q, 5);
+  //   q = ll.push(q, 4);
+  //   ll.printList(q);
+  //   q =ll.zipLists(p, q);
+  //   ll.printList(p);
+  //   ll.printList(q);
 
-//     expect(ll.printList()).toEqual(/*1
-//     2
-//     3
-//     <br>
-//     4
-//     5
-//     6
-//     7
-//     8
-//     <br>
-//     1
-//     4
-//     2
-//     5
-//     3
-//     6
-//     <br>
-//     7
-//     8
-//     <br> */);
-//   });
-// });
+  //   expect(ll.printList()).toEqual(/*1
+  //   2
+  //   3
+  //   <br>
+  //   4
+  //   5
+  //   6
+  //   7
+  //   8
+  //   <br>
+  //   1
+  //   4
+  //   2
+  //   5
+  //   3
+  //   6
+  //   <br>
+  //   7
+  //   8
+  //   <br> */);
+  // });
+});
 
