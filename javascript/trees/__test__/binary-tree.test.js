bd5e7803314cb98fe5c31a6cf784ac63d30e0ee3
Anas Abuobaid <abuobaid.anas1999@gmail.com>
2022-07-03 21:50:45 +0300

    Trees

---
+++ javascript/trees/__test__/binary-tree.test.js
@@ -0,0 +1,63 @@
+'use strict';
+const BinaryTree = require('../lib/binary-tree');
+const BinarySearchTree = require('../lib/bst');
+const Node = require('../lib/node');
+
+let tree = null;
+let searchTree =null;
+describe('Binary Tree', () => {
+  beforeAll(() => {
+    let one = new Node(1);
+    let two = new Node(2);
+    let three = new Node(3);
+    let four = new Node(4);
+    let five = new Node(5);
+    let six = new Node(6);
+    let seven = new Node(7);
+    let eight = new Node(8);
+    let nine = new Node(9);
+    one.left = two;
+    one.right = three;
+    two.left = six;
+    six.right = seven;
+    seven.left = eight;
+    seven.right = nine;
+    three.left = four;
+    three.right = five;
+    tree = new BinaryTree(one);
+    searchTree =new BinarySearchTree(one);
+  });
+
+  test('Can successfully instantiate an empty tree', () => {
+    const newTree = new BinaryTree();
+    expect(newTree.root).toBeNull();
+    expect(tree.root.value).toEqual(1);
+  });
+  test('Can successfully instantiate an empty tree', () => {
+    expect(tree.root.value).toEqual(1);
+  });
+  // test('can successfully add a left child and right child properly to a node', () => {
+  //   let expectedOutput1 = [2];
+  //   let expectedOutput2 = [3];
+  //   let left=searchTree.left();
+  //   let right=searchTree.right();
+  //   expect(left).toEqual(expectedOutput1);
+  //   expect(right).toEqual(expectedOutput2);
+
+  // });
+  test('preOrder', () => {
+    let expectedOutput = [1, 2, 6, 7, 8, 9, 3, 4, 5];
+    let preOrder = tree.preOrder();
+    expect(preOrder).toEqual(expectedOutput);
+  });
+  test('inOrder', () => {
+    let expectedOutput = [6, 8, 7, 9, 2, 1, 4, 3, 5];
+    let inOrder = tree.inOrder();
+    expect(inOrder).toEqual(expectedOutput);
+  });
+  test('postOrder', () => {
+    let expectedOutput = [8, 9, 7, 6, 2, 4, 5, 3, 1];
+    let postOrder = tree.postOrder();
+    expect(postOrder).toEqual(expectedOutput);
+  });
+});
